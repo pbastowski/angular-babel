b0afcbc2728b6f2c7b1e82df5918a55c444b1896
Paul Bastowski <pbastowski@gmail.com>
2015-10-29 20:34:57 +0100

    Added in-memory caching. Added support for { transpile: false } in the package definition, because we don't want to transpile Babel's runtime files.

--- package.js
+++ package.js
@@ -1,33 +1,38 @@
 Package.describe({
-  name: "angular-babel",
-  summary: "Babel compiler and ng-annotate all in the one package for Meteor 1.2+",
-  version: "0.0.1",
-  git: "https://github.com/pbastowski/angular-babel.git"
+    name:    "angular-babel",
+    summary: "Babel compiler and ng-annotate all in the one package for Meteor 1.2+",
+    version: "0.0.1",
+    git:     "https://github.com/pbastowski/angular-babel.git"
 });
 
 Package.registerBuildPlugin({
-  name: 'compileNGScript',
-  sources: [
-    'plugin/ng-script-compiler.js'
-  ],
-  npmDependencies: {
-    'babel-core':  '5.8.25',
-    'ng-annotate': '1.0.2'
-  }
+    name:            'compileNGScript',
+    sources:         [
+        'plugin/ng-script-compiler.js'
+    ],
+    npmDependencies: {
+        'babel-core':  '5.8.25',
+        'ng-annotate': '1.0.2'
+    }
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.2.0.1');
+    api.versionsFrom('METEOR@1.2.0.1');
 
-  api.use('isobuild:compiler-plugin@1.0.0'); // Used for compilers
+    api.use('isobuild:compiler-plugin@1.0.0'); // Used for compilers
 
-  // Files to load in Client only.
-  api.add_files([
-    // Babel files
-    'lib/core-js-no-number.js',
-    'lib/runtime.js',
-  ], 'client');
+    // Files to load in Client only.
+    api.addFiles([
+
+        // Babel files
+        'lib/core-js-no-number.js',
+        'lib/runtime.js',
+
+        // Need this to support es7 async functions and generators
+        '.npm/plugin/compileNGScript/node_modules/babel-core/browser-polyfill.js',
+
+    ], 'client', { transpile: false });
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
 });
--- plugin/ng-script-compiler.js
+++ plugin/ng-script-compiler.js
@@ -1,80 +1,104 @@
-var to5 = Npm.require('babel-core');
-var fs = Npm.require('fs');
-var path = Npm.require('path');
-
+var babel = Npm.require('babel-core');
 var ngAnnotate = Npm.require('ng-annotate');
 
-Object.merge = function (destination, source) {
-  for (var property in source)
-    destination[property] = source[property];
-  return destination;
-}
-
 var config = {
-  // print loaded config
-  "debug":      false,
-  // print active file extensions
-  "verbose":    true,
-  // experimental ES7 support
-  "stage":      0,
-  // module format to use
-  "modules":    'common'
+    // print loaded config
+    'debug':   false,
+    
+    // print active file extensions
+    'verbose': true,
+    
+    // experimental ES7 support
+    'stage':   0,
+    
+    // what module system to use
+    'modules': 'common',
+    
+    // When Babel adds use-strict it kills Meteor's "global" objects
+    // that is, those declared in a file with out var. So, we blacklist it.
+    blacklist: ['useStrict'],
 }
 
-var processFiles = function(files) {
-  files.forEach(processFile);
+var processFiles = function (files) {
+    console.log('\nBabel compiling files:');
+    files.forEach(processFile);
 };
 
-var processFile = function(file) {
+var fileContentsCache = {  };
 
-  var source = file.getContentsAsString();
-  var outputFile = file.getPathInPackage();
-  var to5output = "";
+var processFile = function (file) {
 
-  console.log('Babel compiling: ', outputFile);
+    var inputFile = file.getPathInPackage();
+    var source = file.getContentsAsString();
+    var outputFile = Plugin.convertToStandardPath(file.getPathInPackage());
+    var moduleName = outputFile.replace('.js', '');
+    var output = "";
 
-  try {
-    to5output = to5.transform(source, {
-      // The blacklisting of "userStrict" is required to support
-      // Meteor's file level declarations that Meteor can export
-      // from packages.
-      blacklist: ["useStrict"],
-      sourceMap: true,
-      stage:     config.stage,
-      filename:  file.getDisplayPath(),
-      modules:   config.modules
+    // Get file previous and current file contents hashes
+    var lastHash = fileContentsCache[inputFile] && fileContentsCache[inputFile].hash;
+    var currentHash = file.getSourceHash();
+
+    var transpile = file._resourceSlot.inputResource.fileOptions.transpile;
+    transpile = transpile || typeof transpile === 'undefined';
+    
+    // Only compile files that have changed since the last run
+    if ( !lastHash || lastHash !== currentHash ) {
+        
+        if (transpile) {
+            console.log('  ' + inputFile);
+
+            try {
+                output = babel.transform(source, {
+                    // The blacklisting of "userStrict" is required to support
+                    // Meteor's file level declarations that Meteor can export
+                    // from packages.
+                    sourceMap: true,
+                    stage:     config.stage,
+                    filename:  file.getDisplayPath(),
+                    modules:   config.modules,
+                    blacklist: config.blacklist
+                }).code;
+            } catch (e) {
+                console.log(e); // Show the nicely styled babel error
+                return file.error({
+                    message: 'Babel transform error',
+                    line:    e.loc.line,
+                    column:  e.loc.column
+                });
+            }
+
+            var annotated = ngAnnotate(output, {
+                add: true
+            });
+    
+            if (annotated.errors) {
+                throw new Error(annotated.errors.join(': \n\n'));
+            }
+            
+            output = annotated.src;
+        
+        } else {
+            output = source;
+        }
+        
+        // Update the code cache
+        fileContentsCache[inputFile] = {hash: currentHash, code: output};
+
+    } else {
+        // Pull the code from the cache
+        output = fileContentsCache[inputFile].code ;
+    }
+
+    file.addJavaScript({
+        data: output,
+        path: outputFile
     });
-  } catch (e) {
-    console.log(e); // Show the nicely styled babel error
-    return file.error({
-      message: 'Babel transform error',
-      line:    e.loc.line,
-      column:  e.loc.column
-    });
-  }
-
-  //var annotated = { src: to5output.code };
-
-  //to5output = source;
-
-  var annotated = ngAnnotate(to5output.code, {
-    add: true
-  });
-
-  if (annotated.errors) {
-    throw new Error(annotated.errors.join(': \n\n'));
-  }
-
-  file.addJavaScript({
-    data: annotated.src,
-    path: outputFile
-  });
 };
 
 Plugin.registerCompiler({
-  extensions: ['js'],
-  filenames: []
+    extensions: ['js'],
+    filenames:  []
 
-}, function() {
-  return { processFilesForTarget: processFiles };
+}, function () {
+    return {processFilesForTarget: processFiles};
 });
